61e8b6ebe927f9a3868a1c4db0229bce8882ad70
justin-tse <xiefahit@gmail.com>
2021-07-12 15:30:03 +0800

    add concat

--- miao/lodash/justin_tse-lodash.js
+++ miao/lodash/justin_tse-lodash.js
@@ -1,5 +1,6 @@
 var justin_tse = function () {
-  //Array: https://lodash.com/docs/4.17.15#chunk
+  /* Lodash: Array */
+  //https://lodash.com/docs/4.17.15#chunk
   function chunk(arr, n) {
     let result = [];
     let temp = [];
@@ -22,6 +23,7 @@ var justin_tse = function () {
     return result;
   }
 
+  // https://lodash.com/docs/4.17.15#compact
   function compact(arr, n) {
     let result = [];
     for (let i = 0; i < arr.length; i++) {
@@ -34,8 +36,31 @@ var justin_tse = function () {
     return result;
   }
 
+  // https://lodash.com/docs/4.17.15#concat
+  function concat(arr, ...args) {
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+      result.push(arr[i]);
+    }
+    
+    args = Array.from(arguments).slice(1);
+    for (let i = 0; i < args.length; i++) {
+      let item = args[i];
+      if (Array.isArray(item)) {
+        for (let j = 0; j < item.length; j++) {
+          result.push(item[j]);
+        }
+      } else {
+        result.push(item);
+      }
+    }
+
+    return result;
+  }
+
   return {
     chunk: chunk,
     compact: compact,
+    concat: concat,
   }
 }()
