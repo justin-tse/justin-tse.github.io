9cf17b415bdaf839a445807a57006df95c0f11fe
justin-tse <xiefahit@gmail.com>
2021-09-20 21:55:22 +0800

    add cookie

--- miao/project/bbs/app.js
+++ miao/project/bbs/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const fs = require('fs');
+const cookieParser = require('cookie-parser');
 
 const port = 8008;
 const app = express();
@@ -30,6 +31,7 @@ app.use((req, res, next) => {
   next();
 })
 
+app.use(cookieParser('cookie sign secert'))
 app.use(express.static(__dirname + '/static'));
 app.use(express.json());
 app.use(express.urlencoded()); 
@@ -49,7 +51,7 @@ app.route('/register')
 .get((req, res, next) => {
   res.sendFile(__dirname + '/static/register.html');
 })
-.post((req, res, next) => {
+  .post((req, res, next) => {
   var regInfo = req.body;
   if (users.some(it => it.name == regInfo.name || users.some(it => it.email == regInfo.email))) {
     res.status(400).end('username or email already exists...');
@@ -66,10 +68,17 @@ app.route('/login')
 })
 .post((req, res, next) => {
   var loginInfo = req.body;
-  if (users.some(it => it.name == loginInfo.name || users.some(it => it.password == loginInfo.password))) {
-    res.end('login success...');
+  var  user = users.find(it => it.name == loginInfo.name && it.password == loginInfo.password);
+  if (user) {
+    //  res.clearCookie('loginUser')// when logout, we should clear Cookie
+    res.cookie('loginUserName', user.name, {
+      signed: true
+      // maxAge: 8640000,
+      // expires: new Date(),
+      // httpOnly: true
+    });
+    res.end('login success!');
   } else {
-    users.push(req.body);
     res.end('username or password incorrect...');
   }
 })
@@ -80,8 +89,12 @@ app.route('/post')
   })
   .post((req, res, next) => {
     var postInfo = req.body;
+    var userName = req.signedCookies.loginUserName;
+
     postInfo.timeStamp = new Date().toISOString();
     postInfo.id = posts.length;
+    postInfo.postedBy = userName;
+      
     posts.push(postInfo);
     res.end('post success, post id is ' + postInfo.id);
   })
@@ -90,6 +103,7 @@ app.get('/post/:id', (req, res, next) => {
   var postId = req.params.id;
   var post = posts.find(it => it.id == postId);
   if (post) {
+    res.setHeader('Content-Type', 'text/html; charset=UTF-8')
     res.end(`
       <h1>${post.title}</h1>
       <fieldset>${post.content}</fieldset>
@@ -98,7 +112,6 @@ app.get('/post/:id', (req, res, next) => {
     res.end('404 post not found');
   }
 })
-
 
 app.listen(port, () => {
   console.log('listening on port', port);
